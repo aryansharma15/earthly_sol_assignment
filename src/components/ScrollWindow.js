79ee09c46d0169bdb74b1205116387b1ea854f0d
Aryan Sharma <65057388+aryansharma15@users.noreply.github.com>
2023-02-10 21:11:25 +0530

    added images, working on scroll-win

--- src/components/ScrollWindow.js
+++ src/components/ScrollWindow.js
@@ -1,12 +1,42 @@
 import React from 'react'
 import '../styles/scroll-sec.css'
 
+function reveal(){
+
+}
+
 function ScrollWindow() {
   return (
     <div className="scroll-win">
-        
+      <div>
+        <img src="../assets/kit.webp" alt="Kit" />
+      </div>
+
+      <div>
+        <img src="../assets/tumbler.webp" alt="Tumbler 1" />
+      </div>
+
+      <div>
+        <img src="../assets/tumbler_1.webp" alt="Tumbler 2" />
+      </div>
+
+      <div>
+        <img src="../assets/cotton_eartips.webp" alt="Cotton Eartips" />
+      </div>
+
+      <div>
+        <img src="../assets/loofah.webp" alt="Loofah" />
+      </div>
+
+      <div>
+        <img src="../assets/straws.webp" alt="Straws" />
+      </div>
+
+      <div>
+        <img src="../assets/brush.webp" alt="Brushes" />
+      </div>
     </div>
-  )
+  );
 }
 
 export default ScrollWindow
